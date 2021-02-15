e60375127553fc9f3c8e8fe7f898271477608eb5
apfesta <andy_festa@yahoo.com>
2021-02-15 06:56:43 -0500

    added multi parts to tweet form

--- src/main/java/net/mixednutz/api/twitter/model/TweetForm.java
+++ src/main/java/net/mixednutz/api/twitter/model/TweetForm.java
@@ -5,7 +5,14 @@ import twitter4j.StatusUpdate;
 
 public class TweetForm implements IPost {
 	
+	private static final int MAX_TEXT_SIZE = 110;
+	
 	String status;
+	
+	//Tweet Builder
+	String textPart;
+	String urlPart;
+	String[] tagsPart;
 	
 	public TweetForm() {
 		super();
@@ -17,8 +24,31 @@ public class TweetForm implements IPost {
 	}
 
 	public StatusUpdate toStatusUpdate() {
-		StatusUpdate statusUpdate = new StatusUpdate(status);
-		return statusUpdate;
+		if (status!=null) {
+			return new StatusUpdate(status);
+		}
+		if (textPart!=null && urlPart==null) {
+			return new StatusUpdate(textPart);
+		}
+		//Shorten text
+		StringBuffer buffer = new StringBuffer();
+		if (textPart.length()>MAX_TEXT_SIZE) {
+			buffer.append(textPart.substring(0, MAX_TEXT_SIZE-3))
+				.append("...");
+		} else {
+			buffer.append(textPart);
+		}
+		buffer.append(" ").append(urlPart);
+		if (tagsPart!=null) {
+			for (String tag: tagsPart) {
+				if (tag.startsWith("#")) {
+					buffer.append(" ").append(tag);
+				} else {
+					buffer.append(" #").append(tag);
+				}
+			}
+		}
+		return new StatusUpdate(buffer.toString());
 	}
 
 	public String getStatus() {
@@ -31,6 +61,21 @@ public class TweetForm implements IPost {
 	
 	public void setComposeBody(String status) {
 		setStatus(status);
+	}
+
+	@Override
+	public void setText(String text) {
+		this.textPart = text;
+		
+	}
+
+	@Override
+	public void setUrl(String url) {
+		this.urlPart = url;
+	}
+
+	public void setTags(String[] tags) {
+		this.tagsPart = tags;
 	}
 	
 }
